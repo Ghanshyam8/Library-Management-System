982bfd2479ce984ab44d671647fe6307196d0ef7
Ghanshyam <anishpanta192@gmail.com>
2026-08-02 08:21:20 +0545

    Added View, Edit and Delete feature

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,15 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('addauthor/', views.add_author, name='addauthor'),
+    path('updateauthor/<int:pk>/', views.update_author, name='editauthor'),
+    path('deleteauthor/<int:pk>/', views.delete_author, name='deleteauthor'),
+    path('viewauthor/<int:pk>/', views.view_author, name='viewauthor'),
+    
+    path("search/", views.search_books, name="search"),
+    
     path('addbook/', views.add_book, name='addbook'),
+    path('updatebook/<int:pk>/', views.update_book, name='editbook'),
+    path('viewbook/<int:pk>/', views.view_book, name='viewbook'),
+    path('deletebook/<int:pk>/', views.delete_book, name='deletebook'),
+    
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
-from .forms import mainAuthor, BookStore, Author, Books
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import mainAuthor, BookStore
+from . models import Author, Books
+from django.db.models import Q
+
 
 
 # Create your views here.
@@ -9,6 +12,20 @@ def home(request):
     return render(request, 'home.html', {'authors':authors, 'books':books,})
 
 
+def search_books(request):
+    query = request.GET.get("q", "")
+
+    books = Books.objects.filter(
+        Q(title__icontains=query) |
+        Q(author__name__icontains=query)
+    )
+
+    return render(request, "search_results.html", {
+        "books": books,
+        "query": query,
+    })
+    
+    
 def add_author(request):
     if request.method=="POST":
         form=mainAuthor(request.POST, request.FILES)
@@ -18,11 +35,34 @@ def add_author(request):
         
     else:
         form=mainAuthor()
-    return render(request, 'addAuthor.html', {'authors':form})
+    return render(request, 'authors/addAuthor.html', {'form':form})
 
 
+def update_author(request, pk):
+    author_record=get_object_or_404(Author, id=pk)
+    if request.method=="POST":
+        form=mainAuthor(request.POST, instance=author_record)
+        if form.is_valid():
+            form.save()
+            return redirect('home')    
+    else:
+        form=mainAuthor(instance=author_record)
+    return render(request, 'authors/addAuthor.html', {'form':form})
+
+def delete_author(request, pk):
+    author_record=get_object_or_404(Author, id=pk)
+    if request.method=="POST":
+        author_record.delete()
+        return redirect('home')
+    return render(request, 'authors/confirm.html', {'form':author_record})
+        
+
+def view_author(request, pk):
+    author_record=get_object_or_404(Author, id=pk)
+    return render(request, 'authors/viewauthor.html', {'author':author_record})
 
 
+#BOOK PART
 
 def add_book(request):
     if request.method=="POST":
@@ -34,3 +74,29 @@ def add_book(request):
     else:
         form=BookStore()
     return render(request,'books/addbook.html', {'books':form})
+
+
+def update_book(request, pk):
+    book_records=get_object_or_404(Books, id=pk)
+    if request.method=="POST":
+        form=BookStore(request.POST, instance=book_records)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=BookStore(instance=book_records)
+    return render(request, 'books/addbook.html', {'books':form})
+
+
+def view_book(request, pk):
+    book_records=get_object_or_404(Books, id=pk) 
+    return render(request, 'books/viewbook.html', {'book':book_records})
+
+
+def delete_book(request, pk):
+    book_records=get_object_or_404(Books, id=pk) 
+    if request.method=="POST":
+        book_records.delete()
+        return redirect('home')
+    
+    return render(request, 'books/confirm.html', {'book':book_records})
